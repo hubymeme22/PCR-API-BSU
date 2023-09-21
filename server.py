ba4a1a5e56c73e78ca84fff2c59c54671392e9a4
HueHueberry <huby.new1112@gmail.com>
2023-09-21 23:08:54 +0800

    implemented request templates

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append('routes')
 
 from flask import Flask
+import LoginRoutes
 import Routes
 
 app = Flask(__name__)
@@ -10,6 +11,34 @@ app = Flask(__name__)
 #  Implementation of routes from Routes module  #
 #################################################
 @app.route('/')
-def root(): return Routes.root()
+def root():
+    return Routes.root()
 
+#################################
+#  Admin routes implementation  #
+#################################
+@app.route('/admin/account')
+def adminAccount():
+    return Routes.adminAccount()
+
+@app.route('/admin/account/<id>')
+def adminAccountWithID(id):
+    return Routes.adminAccountWithID(id)
+
+@app.route('/admin/login')
+def adminlogin():
+    return LoginRoutes.adminLogin()
+
+@app.route('/admin/account/create/pmt', methods=['POST'])
+def adminCreateAccountPMT(): return '{}'
+
+@app.route('/admin/account/create/head', methods=['POST'])
+def adminCreateAccountHead(): return '{}'
+
+@app.route('/admin/account/create/indiv', methods=['POST'])
+def adminCreateAccountIndiv(): return '{}'
+
+################################
+#  Head routes implementation  #
+################################
 app.run()
