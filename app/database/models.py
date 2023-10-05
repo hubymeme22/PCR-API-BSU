f25b9eddc48f69cf930eb13e0dfaa6ef55b1ab46
Darren Malibiran <darrenmalibiran@outlook.com>
2023-10-06 06:57:51 +0800

    Created offices and targets models

--- app/database/models.py
+++ app/database/models.py
@@ -1,8 +1,4 @@
-from mongoengine import Document, StringField, IntField
-
-# class Person(Document):
-#     name = StringField(max_length=50, required=True)
-#     age = IntField()
+from mongoengine import Document, EmbeddedDocument, EmbeddedDocumentField, StringField, IntField, FloatField, ListField
 
 class Accounts(Document):
     name = StringField(max_length=50, required=True)
@@ -10,3 +6,25 @@ class Accounts(Document):
     email = StringField(max_length=50, required=True)
     password = StringField(max_length=50, required=True)
     permission = StringField(max_length=50, required=True)
+
+
+# Target model
+class Success(EmbeddedDocument):
+    indicator = StringField()
+    budget = FloatField()
+    division = StringField()
+    accomplishment = StringField()
+    rating = ListField(IntField())
+    remarks = ListField(StringField())
+    assigned_to = ListField(StringField())
+
+class Targets(Document):
+    name = StringField()
+    success = ListField(EmbeddedDocumentField(Success))
+
+
+# Offices model
+class Offices(Document):
+    name = StringField()
+    head = StringField()
+    opcr = ListField(StringField())
