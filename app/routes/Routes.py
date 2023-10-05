d2117ac65fdd478044705b554016645f18e99539
Darren Malibiran <darrenmalibiran@outlook.com>
2023-10-06 07:54:45 +0800

    Created routes for read/write of OPCR/Campus records

--- app/routes/Routes.py
+++ app/routes/Routes.py
@@ -11,7 +11,7 @@
 '''
 from flask import request
 from app import app
-from app.database.models import Accounts, Targets, Offices
+from app.database.models import Accounts, Campuses, OPCR
 import json
 
 @app.route("/")
@@ -70,6 +70,7 @@ def adminCreateAccountPMT():
         username=accountDetails['username'],
         email=accountDetails['email'],
         password=accountDetails['password'],
+        superior = accountDetails.get('superior'),
         permission='pmt'
     )
     new_account.save()
@@ -121,68 +122,103 @@ def adminCreateAccountIndiv():
 
 
 # create new target record (sample route only for testing purposes)
-@app.route('/create/target', methods = ["POST"])
-def create_target():
+@app.route('/create/opcr', methods = ["POST"])
+def create_opcr():
     data = request.get_json(force=True)
     # data = {
-    #     "name": "some name",
-    #     "success": [
+    #     "targets": [
     #         {
-    #             "indicator": "Indicator 1",
-    #             "budget": 1000.5,
-    #             "division": "division 1",
-    #             "accomplishment": "accomplishment 1",
-    #             "rating": [1, 0, 1, 0, 0],
-    #             "remarks": ["comment 1", "comment 2", "comment 3"],
-    #             "assigned_to": ["01a", "01b", "01c"]
-    #         },
-    #         {
-    #             "indicator": "Indicator 2",
-    #             "budget": 2000.5,
-    #             "division": "division 2",
-    #             "accomplishment": "accomplishment 2",
-    #             "rating": [1, 0, 1, 0, 0],
-    #             "remarks": ["comment 1", "comment 2", "comment 3"],
-    #             "assigned_to": ["01a", "01b", "01c"]
+    #             "name": "some name",
+    #             "success": [
+    #                 {
+    #                     "indicator": "Indicator 1",
+    #                     "budget": 1000.5,
+    #                     "division": "division 1",
+    #                     "accomplishment": "accomplishment 1",
+    #                     "rating": [1, 0, 1, 0, 0],
+    #                     "remarks": ["comment 1", "comment 2", "comment 3"],
+    #                     "assigned_to": ["01a", "01b", "01c"]
+    #                 },
+    #                 {
+    #                     "indicator": "Indicator 2",
+    #                     "budget": 2000.5,
+    #                     "division": "division 2",
+    #                     "accomplishment": "accomplishment 2",
+    #                     "rating": [1, 0, 1, 0, 0],
+    #                     "remarks": ["comment 1", "comment 2", "comment 3"],
+    #                     "assigned_to": ["01a", "01b", "01c"]
+    #                 }
+    #             ]
     #         }
-    #     ]
+    #     ],
+    #     "accepted": False,
+    #     "owner": "1231njzxdq4"
     # }
 
-    target = Targets(**data)
-    target.save()
+    opcr_record = OPCR(**data)
+    opcr_record.save()
 
     return {
-        'message': "New Target created",
-        "id": str(target.id)
+        'message': "New OPCR record created",
+        "id": str(opcr_record.id)
     }
 
-# read the target in db based on name attribute (sample route only for testing purposes)
-@app.route('/get/target/<name>', methods = ['GET'])
-def get_target(name):
-    target = Targets.objects(name=name).first()
+# Get all the OPCR records
+@app.route('/get/opcr', methods = ['GET'])
+def get_opcr():
+    target = OPCR.objects()
     if target:
         result = target.to_json()
         return json.loads(result)    
-    return {'message': 'target not found'}
+    return {'message': 'No OPCR record found'}
+
+# Get an OPCR record based on ID
+@app.route('/get/opcr/<id>', methods = ['GET'])
+def opcr_by_id(id):
+    target = OPCR.objects(id=id).first()
+    if target:
+        result = target.to_json()
+        return json.loads(result)    
+    return {'message': 'OPCR record not found'}
 
 
-# create office record (sample route only for testing purposes)
-@app.route('/create/office', methods = ["POST"])
-def create_office():
+# create campus record (sample route only for testing purposes)
+@app.route('/create/campus', methods = ["POST"])
+def create_campus():
     data = request.get_json(force=True)
-    office = Offices(**data)
-    office.save()
+    # data = {
+    #     'name': 'sample campus',
+    #     'offices': [
+    #         {
+    #             'name': 'sample office #1',
+    #             'head': 'sample head #1',
+    #             'opcr': ['id1', 'id2']
+    #         }
+    #     ]
+    # }
+    campus = Campuses(**data)
+    campus.save()
     return {
-        'message': "Office created",
-        'id': str(office.id)
+        'message': "Campus created",
+        'id': str(campus.id)
     }
 
-# read office in db based on name attribute (sample route only for testing purposes)
-@app.route('/get/office/<name>', methods = ['GET'])
-def get_office(name):
-    office = Offices.objects(name=name).first()
-    if office:
-        return json.loads(office.to_json())
-    return {'message': 'office not found'}
+
+
+# read campus in db 
+@app.route('/get/campus', methods=['GET'])
+def get_campus():
+    campuses = Campuses.objects()
+    if campuses:
+        return json.loads(campuses.to_json())
+    return {'message': 'No campus record found'}
+
+# read campus in db based on id attribute (sample route only for testing purposes)
+@app.route('/get/campus/<id>', methods = ['GET'])
+def campus_by_id(id):
+    campus_record = Campuses.objects(id=id).first()
+    if campus_record:
+        return json.loads(campus_record.to_json())
+    return {'message': 'Campus not found'}
 
     
