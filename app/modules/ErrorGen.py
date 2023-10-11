9d3d512a545dc6fad30ad1b706845965831139cb
HueHueberry <huby.new1112@gmail.com>
2023-10-11 12:56:53 +0800

    modified error generation, and added parameter checking

--- app/modules/ErrorGen.py
+++ app/modules/ErrorGen.py
@@ -2,10 +2,17 @@
 In this section, different response generated errors will be defined
 '''
 
-from flask import jsonify
+# error response generation
+def invalidRequestError(datavalue='', error='InvalidRequest', statusCode=400):
+    return ({'data': datavalue, 'error': error}, statusCode)
 
-def invalidRequestError(datavalue='', error='InvalidRequest'):
-    return jsonify({
-        'data': datavalue,
-        'error': error
-    })
+# checks for the request missing parameters
+def parameterCheck(requiredParams: list, jsonRequest: dict):
+    if (requiredParams == None): return ['NoParamsProvided']
+    requestKeys = jsonRequest.keys()
+    missingParams = []
+
+    for key in requiredParams:
+        if (key not in requestKeys):
+            missingParams.append(key)
+    return missingParams
