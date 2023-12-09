7c295fcf45ac9761e878a7bfd35044a969cdaf5d
HueHueberry <huby.new1112@gmail.com>
2023-12-09 13:24:27 +0800

    try-catch for error display added

--- app/module_v2/PMT_v2.py
+++ app/module_v2/PMT_v2.py
@@ -1,5 +1,5 @@
 from app.database.dbConnection import PMTFunctionalities, CampusesFunctionalities, OPCRFunctionalities
-from app.modules import Sessions
+from app.modules import Sessions, ErrorGen
 from app.module_v2 import Utils
 from flask import request
 
@@ -8,39 +8,51 @@ def getOpcrReport():
     if (tokenStatus != None):
         return tokenStatus
 
-    userInfos = Sessions.getSessionInfo(request.headers.get('Authorization'))
-    pmtCampus = CampusesFunctionalities.getPmtCampus(userInfos['userid'])
+    try:
+        userInfos = Sessions.getSessionInfo(request.headers.get('Authorization'))
+        pmtCampus = CampusesFunctionalities.getPmtCampus(userInfos['userid'])
 
-    pmtList = []
-    for office in pmtCampus['offices']:
-        if (len(office['opcr']) > 0):
-            opcrData = OPCRFunctionalities.getOneOpcr(office['opcr'][0].__str__())
-            pmtList.append({
-                '_id': {'$oid': office['_id'].__str__()},
-                'name': office['name'],
-                'status': opcrData['status']
-            })
+        pmtList = []
+        for office in pmtCampus['offices']:
+            if (len(office['opcr']) > 0):
+                opcrData = OPCRFunctionalities.getOneOpcr(office['opcr'][0].__str__())
+                pmtList.append({
+                    '_id': {'$oid': office['_id'].__str__()},
+                    'name': office['name'],
+                    'status': opcrData['status']
+                })
 
-    return {
-        'data': pmtList,
-        'error': None
-    }
+        return {
+            'data': pmtList,
+            'error': None
+        }
+
+    except Exception as e:
+        return ErrorGen.invalidRequestError(
+            error=str(e),
+            statusCode=500)
 
 def getOpcrByOfficeId(officeid: str):
     tokenStatus = Sessions.requestTokenCheck('pmt')
     if (tokenStatus != None):
         return tokenStatus
 
-    opcrData = PMTFunctionalities.getOpcrDataByOfficeID(officeid)
-    opcrData = Utils.convertToLegacy(opcrData)
-    for it, target in enumerate(opcrData['targets']):
-        updatedTarget = Utils.convertToLegacy(target)
-        for si, success in enumerate(updatedTarget['success']):
-            updatedTarget['success'][si] = Utils.convertLegacyToString(success, 'oid')
-        opcrData['targets'][it] = updatedTarget
-        opcrData['owner'] = opcrData['owner'].__str__()
+    try:
+        opcrData = PMTFunctionalities.getOpcrDataByOfficeID(officeid)
+        opcrData = Utils.convertToLegacy(opcrData)
+        for it, target in enumerate(opcrData['targets']):
+            updatedTarget = Utils.convertToLegacy(target)
+            for si, success in enumerate(updatedTarget['success']):
+                updatedTarget['success'][si] = Utils.convertLegacyToString(success, 'oid')
+            opcrData['targets'][it] = updatedTarget
+            opcrData['owner'] = opcrData['owner'].__str__()
 
-    return {
-        'data': opcrData,
-        'error': None
-    }
+        return {
+            'data': opcrData,
+            'error': None
+        }
+
+    except Exception as e:
+        return ErrorGen.invalidRequestError(
+            error=str(e),
+            statusCode=500)
